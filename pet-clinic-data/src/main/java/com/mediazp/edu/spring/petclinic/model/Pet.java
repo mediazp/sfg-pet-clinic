16d5e1abdc321a6c36c264ad9de47d1434e9ffb2
Vlad Medianyk <medianik@gmail.com>
2018-10-02 13:27:18 +0300

    updated visits with JPA entity mappings. Closes #38

--- pet-clinic-data/src/main/java/com/mediazp/edu/spring/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/mediazp/edu/spring/petclinic/model/Pet.java
@@ -6,9 +6,13 @@
 package com.mediazp.edu.spring.petclinic.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
@@ -20,13 +24,18 @@ import javax.persistence.Table;
 public class Pet extends BaseEntity {
 
     private String name;
+    
     @ManyToOne
     @JoinColumn(name = "type_id")
     private PetType petType;
+    
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
     private LocalDate birthDate;
+    
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -60,4 +69,13 @@ public class Pet extends BaseEntity {
         this.birthDate = birthDate;
     }
 
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
+    }
+
+    
 }
--- pet-clinic-data/src/main/java/com/mediazp/edu/spring/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/mediazp/edu/spring/petclinic/model/Visit.java
@@ -6,16 +6,28 @@
 package com.mediazp.edu.spring.petclinic.model;
 
 import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 /**
  *
  * @author Vlad
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
     
+    @Column(name = "date")
     private LocalDate date;
+    @Column(name = "description")
     private String description;
-    private Pet pet;;
+    
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
 
     public LocalDate getDate() {
         return date;
