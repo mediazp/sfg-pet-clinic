4c9958289c601b17db45466ba38dfa5df3755f9a
Vlad Medianyk <medianik@gmail.com>
2018-10-02 13:45:46 +0300

    implemented repos. Closes #39

---
+++ pet-clinic-data/src/main/java/com/mediazp/edu/spring/petclinic/repositories/OwnerRepository.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mediazp.edu.spring.petclinic.repositories;
+
+import com.mediazp.edu.spring.petclinic.model.Owner;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Vlad
+ */
+public interface OwnerRepository extends CrudRepository<Owner, Long>{
+    
+}
